a2460009269fd0139be4bc2b060fb64be75cdde9
Irina D <RabotaYd2021@yandex.ru>
2024-07-16 16:47:11 +0300

    Сделаны две задачи

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,38 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let arr = [];
+	let d = Math.pow(b, 2) - 4 * a * c;
+
+	if (d > 0) {
+		arr[0] = (-b + Math.sqrt(d)) / (2 * a);
+		arr[1] = (-b - Math.sqrt(d)) / (2 * a);
+	} else if (d === 0) {
+		arr[0] = -b / (2 * a);
+	} else if (d < 0) {
+		arr = [];
+	}
+	return arr;	
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	let creditInterest = parseInt(percent) / 12 / 100;
+	let initialPayment = parseInt(contribution);
+	let amountCredit = parseInt(amount);
+	let months = parseInt(countMonths);
+	
+	if (isNaN(creditInterest) || creditInterest < 0) {
+		return (`Параметр "Процентная ставка" содержит неправильное значение ${percent}`);
+	} else if (isNaN(initialPayment) || initialPayment < 0) {
+		return (`Параметр "Первоначальный взнос" содержит неправильное значение ${contribution}`);
+	} else if (isNaN(amountCredit || amountCredit < 0)) {
+		return (`Параметр "Сумма кредита" содержит неправильное значение ${amount}`);
+	} else if (isNaN(months) || months < 0) {
+		return (`Параметр "Срок в месяцах" содержит неправильное значение ${countMonths}`);
+	}
+	
+	let loanBody = amount - contribution;
+	let payment = loanBody * (creditInterest + (creditInterest / (((1 + creditInterest) ** months) - 1)));
+	let totalAmount = (payment * months).toFixed(2);
+	return +totalAmount;  
 }
