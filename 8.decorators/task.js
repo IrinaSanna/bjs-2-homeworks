5f823ba02057d1c7435b062de5a5f9d741eb27e4
Irina D <RabotaYd2021@yandex.ru>
2024-08-18 15:02:39 +0300

    Восьмое задание выполнено наполовину

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,52 @@
+/** @format */
+
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+	let cache = [];
+
+	function wrapper(...args) {
+		let hash = md5(args);
+		let objectInCache = cache.find((item) => item.hash === hash);
+		if (objectInCache) {
+			console.log("Из кеша: " + objectInCache.value, cache);
+			return "Из кеша: " + objectInCache.value;
+		}
+		let result = func(...args);
+		cache.push({
+			hash: hash,
+			value: result
+		});
+		if (cache.length > 5) {
+			cache.shift();
+		}
+		console.log("Вычисляем: " + result, cache);
+		return "Вычисляем: " + result;
+	}
+	return wrapper;
 }
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
+	let timeoutId;
+	let count = 0;
+	let allCount = 0;
+
+	function wrapper(...args) {
+		if (timeoutId) {
+			clearTimeout(timeoutId);
+			allCount++;
+		}
+		if (!timeoutId) {
+			func.apply(this, args);
+			count++;
+		}
+
+    timeoutId = setTimeout(() => {
+		count++;
+		func.apply(this, args);
+		wrapper.count = count;
+	}, delay);
+	wrapper.allCount = allCount;			
+	}
+	return wrapper;
 }
