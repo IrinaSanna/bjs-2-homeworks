ab43eac2b80706731897897641a230b8d008eb31
Irina D <RabotaYd2021@yandex.ru>
2024-08-21 12:55:22 +0300

    Восьмое задание полностью готово

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,52 +1,46 @@
-/** @format */
-
 //Задача № 1
 function cachingDecoratorNew(func) {
-	let cache = [];
+  let cache = [];
 
-	function wrapper(...args) {
-		let hash = md5(args);
-		let objectInCache = cache.find((item) => item.hash === hash);
-		if (objectInCache) {
-			console.log("Из кеша: " + objectInCache.value, cache);
-			return "Из кеша: " + objectInCache.value;
-		}
-		let result = func(...args);
-		cache.push({
-			hash: hash,
-			value: result
-		});
-		if (cache.length > 5) {
-			cache.shift();
-		}
-		console.log("Вычисляем: " + result, cache);
-		return "Вычисляем: " + result;
-	}
-	return wrapper;
+  function wrapper(...args) {
+    let hash = md5(args);
+    let objectInCache = cache.find((item) => item.hash === hash);
+    if (objectInCache) {
+      console.log("Из кеша: " + objectInCache.value, cache);
+      return "Из кеша: " + objectInCache.value;
+    }
+    let result = func(...args);
+    cache.push({ hash: hash, value: result });
+    if (cache.length > 5) {
+      cache.shift();
+    }
+    console.log("Вычисляем: " + result, cache);
+    return "Вычисляем: " + result;
+  }
+  return wrapper;
 }
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-	let timeoutId;
-	let count = 0;
-	let allCount = 0;
-
-	function wrapper(...args) {
-		if (timeoutId) {
-			clearTimeout(timeoutId);
-			allCount++;
-		}
-		if (!timeoutId) {
-			func.apply(this, args);
-			count++;
-		}
+  let timeoutId;
+  
+  function wrapper(...args) {
+    wrapper.allCount++;
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    };
+    if (!timeoutId) {
+      func.call(this, ...args);
+      wrapper.count++;
+    };
 
     timeoutId = setTimeout(() => {
-		count++;
-		func.apply(this, args);
-		wrapper.count = count;
-	}, delay);
-	wrapper.allCount = allCount;			
-	}
-	return wrapper;
+      func.call(this, ...args);
+      wrapper.count++;
+    }, delay);
+  }
+  wrapper.count = 0;
+  wrapper.allCount = 0;
+  
+  return wrapper;
 }
